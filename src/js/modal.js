399b550d355fd50403f82f28f5dc4fa60c7f3d54
Oleksandr Lytvyniuk <sandro132015@gmail.com>
2023-01-04 20:36:04 +0200

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,9 +2,33 @@ const hide = document.querySelector('.backdrop');
 const moadalClose = document.querySelector('.modal__button--close');
 
 moadalClose.addEventListener('click', onCloseModal);
+hide.addEventListener('click', onCloseModalBackdrop);
+document.addEventListener('keydown', onCloseModalEsc);
 
 function onCloseModal(evt) {
   evt.preventDefault();
-
+  console.log(hide.classList);
   hide.classList.add('is-hidden');
+  console.log(hide.classList);
+}
+
+function onCloseModalBackdrop(evt) {
+  evt.preventDefault();
+
+  if (evt.target !== hide) {
+    return;
+  }
+  hide.classList.add('is-hidden');
+  hide.removeEventListener('click', onCloseModalBackdrop);
+}
+
+function onCloseModalEsc(evt) {
+  evt.preventDefault();
+
+  console.log(evt.key);
+  if (evt.key === 'Escape') {
+    hide.classList.add('is-hidden');
+  }
+
+  document.removeEventListener('keydown', onCloseModalEsc);
 }
