e511f30a00451ad20681d8cb0a5a2cd7400abe3a
Denis Mazurov <19bobrik92@gmail.com>
2023-01-09 18:30:11 +0200

    fix bugs html css

--- src/js/render/renderSearchMovies.js
+++ src/js/render/renderSearchMovies.js
@@ -2,6 +2,8 @@ import refs from '../refs';
 
 import MovieDB from '../API/fetchMovieAPI';
 
+import defaultImg from '../../images/default.jpg'
+
 const movieDB = new MovieDB();
 
 async function findGenres(genresID) {
@@ -22,7 +24,7 @@ export default async function renderMovies(data) {
       const genresList = await findGenres(genre_ids);
 
       const poster = `https://image.tmdb.org/t/p/w500/${poster_path}`;
-      const posterPlaceholder = `https://via.placeholder.com/395x574`;
+      const posterPlaceholder = defaultImg;
 
       const genres =
         genresList.length < 3
@@ -30,10 +32,10 @@ export default async function renderMovies(data) {
           : `${genresList[0]}, ${genresList[1]}, Other`;
 
       return `<li class="movies__item" id="${id}">
-        <a href="">
+        <a href="" movies__link>
           <img src="${
             poster_path ? poster : posterPlaceholder
-          }" width="395" height="574" alt="${title}" />
+          }" class="movies__img" alt="${title}" />
           <div class="movies__info">
             <h3 class="movies__title">${title}</h3>
             <p class="movies__text">${
