583df3d99b4982d382a1908b13e415a8d4e9908e
maxyma-sanx <maxymasan.dev@gmail.com>
2023-01-12 13:16:12 +0200

    pagination size fix

--- src/js/API/tuiPaginationAPI.js
+++ src/js/API/tuiPaginationAPI.js
@@ -1,11 +1,17 @@
 export default function (totalResults) {
   // Базові налаштування пагігнації
+  const mediaQuery = window.matchMedia('(max-width: 768px)');
+
   const options = {
     totalItems: totalResults,
     itemsPerPage: 20,
     visiblePages: 5,
     usageStatistics: false,
   };
+
+  if (mediaQuery.matches) {
+    options.visiblePages = 3;
+  }
 
   return options;
 }
