6084fa16c8f594152d06b7fae7d997a389f606c3
maxyma-sanx <maxymasan.dev@gmail.com>
2023-01-13 20:21:03 +0200

    button up (Dmytro Avramenko)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import 'animate.css';
 import './js/API/notiflixAPI';
-
 import renderPopularMovies from './js/render/renderPopularMovies';
 import onSeachFormSubmit from './js/events/form';
 import {
@@ -10,5 +9,5 @@ import {
 } from './js/events/modal';
 import onMovieClick from './js/events/movieCard';
 import themeSwither from './js/events/theme-switcher';
-
+import { trackScroll, сlickToUpBtn } from './js/events/btnUp';
 import { signUpWithGoogle, signOutFromGoogle } from './js/API/firebaseAPI';
--- src/library.js
+++ src/library.js
@@ -1,13 +1,12 @@
 import 'animate.css';
 import './js/API/notiflixAPI';
-
 import {
   onCloseModal,
   onCloseModalBackdrop,
   onCloseModalEsc,
 } from './js/events/modal';
-
 import watchedMovies from './js/events/watched';
 import queueMovies from './js/events/queue';
 import themeSwither from './js/events/theme-switcher';
+import { trackScroll, сlickToUpBtn } from './js/events/btnUp';
 import { signOutFromGoogle } from './js/API/firebaseLibraryAPI';
