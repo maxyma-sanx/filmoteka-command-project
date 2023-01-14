b8871aee812ab9c9fd394fd805bb87c6f29a05e7
maxyma-sanx <maxymasan.dev@gmail.com>
2023-01-15 00:46:12 +0200

    refactor

--- src/js/events/movieCard.js
+++ src/js/events/movieCard.js
@@ -6,7 +6,6 @@ import { Loading, Notify } from 'notiflix';
 import MovieDB from '../API/fetchMovieAPI';
 
 import renderTargetMovie from '../render/renderTargetMovie';
-import renderTrailer from '../render/renderTrailerIframe';
 
 import {
   onCloseModal,
@@ -17,7 +16,6 @@ import {
 
 import checkData from '../utils/checkData';
 import removeLocalData from '../utils/removeData';
-import renderTrailer from '../render/renderTrailerIframe';
 
 refs.movies.addEventListener('click', onMovieClick);
 
@@ -30,13 +28,9 @@ if (!lang) {
   lang = 'en-US';
 }
 
-// Пусті массиви на той випадок, коли localStorage порожній
-const watched = [];
-const queue = [];
-
 // Массиви данних які беруться з localStorage при завантаженні сторінки
-const parsedWatchedData = JSON.parse(localStorage.getItem('watched'));
-const parsedQueueData = JSON.parse(localStorage.getItem('queue'));
+const parsedWatchedData = JSON.parse(localStorage.getItem('watched')) || [];
+const parsedQueueData = JSON.parse(localStorage.getItem('queue')) || [];
 
 // Логіка при відкритті модалки з фільмом
 export default async function onMovieClick(e) {
@@ -75,36 +69,17 @@ export default async function onMovieClick(e) {
     }
 
     trailerBtn.addEventListener('click', () => {
-      // if (results.length === 0) {
-      //   const instance = basicLightbox.create(
-      //     `<div class="modal-iframe">
-      //         <p class="trailer-error">Trailer not found</p>
-      //       </div>`,
-      //     {
-      //       onShow: instance => {
-      //         window.addEventListener('keydown', function onEscClick(e) {
-      //           if (e.code === 'Escape') {
-      //             instance.close();
-      //             window.removeEventListener('keydown', onEscClick);
-      //           }
-      //         });
-      //       },
-      //     }
-      //   );
-
-      //   instance.show();
-      // }
+      const movieID = results.find(item => item.name === 'Official Trailer');
 
       const instance = basicLightbox.create(
         `<iframe class="video-trailer" width="640" height="480" frameborder="0" allowfullscreen allow='autoplay'
             src="https://www.youtube.com/embed/${
-              results[0] ? results[0].key : 'zwBpUdZ0lrQ'
+              movieID ? movieID.key : 'zwBpUdZ0lrQ'
             }?autoplay=1" >
           </iframe>`,
         {
           onShow: instance => {
             window.addEventListener('keydown', function onEscClick(e) {
-              // console.log(e);
               if (e.code === 'Escape') {
                 instance.close();
                 window.removeEventListener('keydown', onEscClick);
@@ -118,40 +93,50 @@ export default async function onMovieClick(e) {
     });
 
     watchedBtn.addEventListener('click', () => {
-      if (watchedBtn.textContent === 'Add to watched') {
-        watchedBtn.textContent = 'Remove from watched';
-        Notify.info('Success! The movie has been added to the library.');
-
-        // Пушимо в localStorage id фільму
-        checkData(parsedWatchedData, watched, filmID, WATCHED_KEY);
-      } else {
-        watchedBtn.textContent = 'Add to watched';
-
-        // Видаляємо з localStorage id фільму
-        removeLocalData(watched, filmID, WATCHED_KEY);
-        removeLocalData(parsedWatchedData, filmID, WATCHED_KEY);
-        Notify.info('Success! The movie has been removed from the library.');
-      }
+      changeWatchedBtn(watchedBtn, filmID);
     });
 
     queueBtn.addEventListener('click', () => {
-      if (queueBtn.textContent === 'Add to queue') {
-        queueBtn.textContent = 'Remove from queue';
-        Notify.info('Success! The movie has been added to the library.');
-
-        // Пушимо в localStorage id фільму
-        checkData(parsedQueueData, queue, filmID, QUEUE_KEY);
-      } else {
-        queueBtn.textContent = 'Add to queue';
-
-        Notify.info('Success! The movie has been removed from the library.');
-
-        // Видаляємо з localStorage id фільму
-        removeLocalData(queue, filmID, QUEUE_KEY);
-        removeLocalData(parsedQueueData, filmID, QUEUE_KEY);
-      }
+      queueWatchedBtn(queueBtn, filmID);
     });
   } catch (error) {
     Notify.failure(`${error.message}`);
+  }
+}
+
+// Функція додає та видаляє з localStorage id переглянутих фільмів
+
+function changeWatchedBtn(btn, filmID) {
+  if (btn.textContent === 'Add to watched') {
+    btn.textContent = 'Remove from watched';
+    Notify.info('Success! The movie has been added to the library.');
+
+    // Пушимо в localStorage id фільму
+    checkData(parsedWatchedData, filmID, WATCHED_KEY);
+  } else {
+    btn.textContent = 'Add to watched';
+
+    // Видаляємо з localStorage id фільму
+    removeLocalData(parsedWatchedData, filmID, WATCHED_KEY);
+    Notify.info('Success! The movie has been removed from the library.');
+  }
+}
+
+// Функція додає та видаляє з localStorage id фільмів у черзі
+
+function queueWatchedBtn(btn, filmID) {
+  if (btn.textContent === 'Add to queue') {
+    btn.textContent = 'Remove from queue';
+    Notify.info('Success! The movie has been added to the library.');
+
+    // Пушимо в localStorage id фільму
+    checkData(parsedQueueData, filmID, QUEUE_KEY);
+  } else {
+    btn.textContent = 'Add to queue';
+
+    Notify.info('Success! The movie has been removed from the library.');
+
+    // Видаляємо з localStorage id фільму
+    removeLocalData(parsedQueueData, filmID, QUEUE_KEY);
   }
 }
