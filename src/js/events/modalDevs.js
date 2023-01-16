98d72130997771d0b253aa184d362a1cdfa3a33f
Maxym Koralkov <maxyma.sanx@gmail.com>
2023-01-16 20:17:14 +0200

    clsBtnModalDev uncomment

--- src/js/events/modalDevs.js
+++ src/js/events/modalDevs.js
@@ -4,12 +4,12 @@ const backdropDevs = document.querySelector('.backdrop_dev');
 const clsBtnModalDev = document.querySelector('.modal_dev_btn--close');
 const openBtnModalDev = document.querySelector('.footer__link');
 
-// clsBtnModalDev.addEventListener('click', onCloseModalDev);
-// function onCloseModalDev() {
-//   console.log('closed modal_dev');
-//   backdropDevs.classList.add('is-hidden');
-//   // removeEventListener();
-// }
+clsBtnModalDev.addEventListener('click', onCloseModalDev);
+function onCloseModalDev() {
+  console.log('closed modal_dev');
+  backdropDevs.classList.add('is-hidden');
+  // removeEventListener();
+}
 
 openBtnModalDev.addEventListener('click', onOpenModalDev);
 function onOpenModalDev() {
