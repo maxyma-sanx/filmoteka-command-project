62dbb37d93787d119ce263413bba4936cc4ed0d6
Maxym Koralkov <maxyma.sanx@gmail.com>
2023-01-18 14:58:34 +0200

    import fix

--- src/js/utils/checkUrkLang.js
+++ src/js/utils/checkUrkLang.js
@@ -1,4 +1,5 @@
 import lang from '../utils/checkStorageLang';
 
 // Перевірка чи обрана мова українська
-export default isUkraineLang = lang === 'uk-UA';
+const isUkraineLang = lang === 'uk-UA';
+export default isUkraineLang;
